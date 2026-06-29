51bac5f79410192ca6acb7046cde4bbd639f13c9
Nilton Silva <niltonsilva.code@gmail.com>
2026-06-29 08:36:15 -0300

    Adicionando time stamps

--- src/ProductManagement.API/obj/Debug/net10.0/ProductManagement.API.AssemblyInfo.cs
+++ src/ProductManagement.API/obj/Debug/net10.0/ProductManagement.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductManagement.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59ac7bd617f1ef57d8cf0540a4b8c989caaf6c7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae3923afa42f27ef846b1ff76799f4bc638c9efa")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductManagement.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductManagement.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
